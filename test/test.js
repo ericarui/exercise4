23ab9d7ad78388d10b554fab348215ee017f8aec
erica <erica.zhang@zhajob.com>
2018-06-27 18:13:06 +0800

    this 关键字

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
+var should = require('should')
 describe('this', function () {
   it('setTimeout', function (done) {
     var obj = {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          //这里obj调用say,所以this指向obj
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,33 +17,49 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      this.should.equal(global)
     }
-    test()
+    test()   //默认指向全局
   })
 
   describe('bind', function () {
     it('bind undefined', function () {
+
+      // 相当于这样的顺序
+      // 1 
+      // var obj;
+      // 2
+      // function _say(){
+      //   this.should.equal(global)
+      // }
+      // 3
+      // _say.bind(obj)    这时obj为undifined
+      // 4 自执行函数执行完之后obj才有值
+      // 5 obj.say()
+
       var obj = {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
           return _say.bind(obj)
-        }()
+        }() 
       }
       obj.say()
     })
+
+
 
     it('bind normal', function () {
       var obj = {}
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // obj已经声明
+          this.should.equal(obj)
         }
-        return _say.bind(obj)
+        return _say.bind(obj)  //这时obj = {}
       }()
       obj.say()
     })
